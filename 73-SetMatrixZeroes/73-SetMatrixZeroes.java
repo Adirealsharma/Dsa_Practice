d5f5c4a22e142b6fbc6d65c79ccfd790f350a959
Aditya Sharma <147475009+Adirealsharma@users.noreply.github.com>
2026-07-26 22:13:58 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -0,0 +1,27 @@
+// Last updated: 7/26/2026, 10:13:58 PM
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean zeroRows[]=new boolean[m];
+        boolean zeroCols[]=new boolean[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    zeroRows[i]= true;
+                    zeroCols[j]= true;
+                    }
+                }
+            }
+            
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(zeroRows[i] || zeroCols[j]){
+                    matrix[i][j]=0;
+                    }
+                }
+            }
+        
+    }
+}
